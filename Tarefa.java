c3bae10a98fbfb96c19bf3a5e5adafa8f2956689
tvaz085-prog <tvaz085@gmail.com>
2026-03-24 17:57:17 -0300

    Update Tarefa.java

--- Tarefa.java
+++ Tarefa.java
@@ -1,1 +1,17 @@
+public class Tarefa {
+    String nome;
+    String descricao;
+    String status;
+    Usuario responsavel;
 
+    public Tarefa(String nome, String descricao, Usuario responsavel) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.responsavel = responsavel;
+        this.status = "Pendente";
+    }
+
+    public void exibir() {
+        System.out.println("Tarefa: " + nome + " - " + status);
+    }
+}
