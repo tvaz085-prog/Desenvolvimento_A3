cf33628324a05d3dc3afd13b6bd2df35999d13f7
tvaz085-prog <tvaz085@gmail.com>
2026-03-24 17:57:46 -0300

    Update Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -1,1 +1,25 @@
+import java.util.ArrayList;
 
+public class Sistema {
+    ArrayList<Usuario> usuarios = new ArrayList<>();
+    ArrayList<Projeto> projetos = new ArrayList<>();
+    ArrayList<Equipe> equipes = new ArrayList<>();
+
+    public void cadastrarUsuario(Usuario u) {
+        usuarios.add(u);
+    }
+
+    public void cadastrarEquipe(Equipe e) {
+        equipes.add(e);
+    }
+
+    public void cadastrarProjeto(Projeto p) {
+        projetos.add(p);
+    }
+
+    public void listarProjetos() {
+        for (Projeto p : projetos) {
+            p.exibir();
+        }
+    }
+}
