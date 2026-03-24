5f68428800ef5595963f706d7c5024ba78c8cc2d
tvaz085-prog <tvaz085@gmail.com>
2026-03-24 17:57:32 -0300

    Update Projeto.java

--- Projeto.java
+++ Projeto.java
@@ -1,1 +1,46 @@
+import java.util.ArrayList;
 
+public class Projeto {
+    String nome;
+    String descricao;
+    String dataInicio;
+    String dataFim;
+    String status;
+    Usuario gerente;
+
+    ArrayList<Equipe> equipes = new ArrayList<>();
+    ArrayList<Tarefa> tarefas = new ArrayList<>();
+
+    public Projeto(String nome, String descricao, String dataInicio, String dataFim, Usuario gerente) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.gerente = gerente;
+        this.status = "Planejado";
+    }
+
+    public void adicionarEquipe(Equipe e) {
+        equipes.add(e);
+    }
+
+    public void adicionarTarefa(Tarefa t) {
+        tarefas.add(t);
+    }
+
+    public void exibir() {
+        System.out.println("\nProjeto: " + nome);
+        System.out.println("Gerente: " + gerente.nome);
+        System.out.println("Status: " + status);
+
+        System.out.println("Equipes:");
+        for (Equipe e : equipes) {
+            e.exibir();
+        }
+
+        System.out.println("Tarefas:");
+        for (Tarefa t : tarefas) {
+            t.exibir();
+        }
+    }
+}
