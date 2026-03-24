ddb6d2da921dae560559ab4dfed054bf3679e998
tvaz085-prog <tvaz085@gmail.com>
2026-03-24 17:57:04 -0300

    Update Equipe.java

--- Equipe.java
+++ Equipe.java
@@ -1,1 +1,23 @@
+import java.util.ArrayList;
 
+public class Equipe {
+    String nome;
+    String descricao;
+    ArrayList<Usuario> membros = new ArrayList<>();
+
+    public Equipe(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public void adicionarMembro(Usuario u) {
+        membros.add(u);
+    }
+
+    public void exibir() {
+        System.out.println("Equipe: " + nome);
+        for (Usuario u : membros) {
+            u.exibir();
+        }
+    }
+}
