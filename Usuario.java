6d637af72dc7918ea9be9986d14130c6fa5a60b6
tvaz085-prog <tvaz085@gmail.com>
2026-03-24 17:56:51 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,1 +1,23 @@
+public class Usuario {
+    String nome;
+    String cpf;
+    String email;
+    String cargo;
+    String login;
+    String senha;
+    String perfil;
 
+    public Usuario(String nome, String cpf, String email, String cargo, String login, String senha, String perfil) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.cargo = cargo;
+        this.login = login;
+        this.senha = senha;
+        this.perfil = perfil;
+    }
+
+    public void exibir() {
+        System.out.println(nome + " - " + perfil);
+    }
+}
